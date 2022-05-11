fa6d7e50738afd062a457fec2f90a411f6567df3
SandeepK1729 <sandeepkumargalipelly@gmail.com>
2022-05-11 14:04:31 +0530

    started week-7

---
+++ week-7/first-fit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    int b; printf("Enter no of blocks : ");  scanf("%d", &n);
+    int blocks[b];
+    printf("Enter %d block sizes : "); 
+    for(int i = 0; i < b; i++) scanf("%d", blocks + i);
+
+    int f; printf("Enter no of files : ");   scanf("%d", &f);
+    int files[f];
+    for(int i = 0; i < n; i++) scanf("%d", files + i);
+
+    
+}
